e2ba05d8a978e8bacba8ed561e732c37650563a3
Plasmath <77946264+Plasmath@users.noreply.github.com>
2023-10-07 23:31:18 -0400

    Fixed small problem where symmetrically-faced polyhedra were not faceted

--- noblefaceting.py
+++ noblefaceting.py
@@ -112,7 +112,8 @@ def noblecheck(p, group, minsize=4): #checks for noble polyhedra within a plane 
         for sym in planesyms: #filter out irremovable exotic faces
             permuted = permute(c, sym)
             permedges = [tuple(sorted([permuted[i],permuted[(i+1)%len(c)]])) for i in range(len(c))]
-            if len(set(edges) & set(permedges)) > 0:
+            if len(set(edges) & set(permedges)) > 0 and set(edges) != set(permedges):
+                print(c,permuted)
                 break
         else:
             equivs = [identdict[e] for e in edges if identdict[e] != []]
